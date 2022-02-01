ab25288ac8aa9fa685b78ea4d51b92946a2180e7
Nabihah01 <nabihah.rashid@rutgers.edu>
2022-02-01 12:46:29 -0500

    Update Patient.java

--- src/vaccine_service/Patient.java
+++ src/vaccine_service/Patient.java
@@ -12,16 +12,29 @@ public class Patient implements Comparable<Patient> {
         this.lname = split[2];
     }
 
+    public String getFname() { return this.fname; }
+
+    public String getLname() { return this.lname; }
+
+    public Date getDob() { return this.dob; }
+
+    public void setFname(String fname) { this.fname = fname; }
+
+    public void setLname(String lname) { this.lname = lname; }
+
+    public void setDob(Date dob) { this.dob = dob; }
+
     @Override
     public String toString() {
         //does it have to be in a specific order?
-        String patient = this.fname + ' ' + this.lname + ' ';
-        //I want to add this.dob.month.toString() but it keeps giving me error
+        String patient = this.fname + ' ' + this.lname + ' ' + this.dob.getMonth()
+                + '/' + this.dob.getDay() + '/' + this.dob.getYear() + ' ';
         return patient;
     }
 
     @Override
     public int compareTo(Patient patient) {
+        int stringCompare = 0;
         //sort by last name, then first, then dob
         if(this.lname.compareTo(patient.lname) == 0) {
             if(this.fname.compareTo(patient.fname) == 0) {
@@ -29,14 +42,20 @@ public class Patient implements Comparable<Patient> {
                     return 0;
                 return this.dob.compareTo(patient.dob);
             }
-            return this.fname.compareTo(patient.fname);
+            stringCompare = this.fname.compareTo(patient.fname);
+            if(stringCompare < 0) return -1;
+            else return 1;
         }
-        return this.lname.compareTo(patient.lname);
+        stringCompare = this.lname.compareTo(patient.lname);
+        if(stringCompare < 0) return -1;
+        else return 1;
     }
 
     public static void main(String[] args) {
         Patient p = new Patient("10/7/2001 Nabihah Rashid");
+        Patient s = new Patient("02/15/2001 Maryam Syed");
         String res = p.toString();
         System.out.println(res);
+        System.out.println(s.compareTo(p));
     }
 }
